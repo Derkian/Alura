4d8183759a9841dcbb4433b71423935ad4cfa218
Derkian <rderkian@gmail.com>
2019-02-08 18:44:56 -0200

    vendor

--- projeto-webpack/client/webpack.config.js
+++ projeto-webpack/client/webpack.config.js
@@ -4,7 +4,6 @@ const extractTextPlugin = require('extract-text-webpack-plugin');
 const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require('webpack');
 
-
 let plugins = [];
 
 plugins.push(new extractTextPlugin('styles.css'));
@@ -14,7 +13,15 @@ plugins.push(new webpack.ProvidePlugin({
     'jQuery' : 'jquery/dist/jquery.js'
 }))
 
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+    name : 'vendor',
+    filename : 'vendor.bundle.js'
+}));
+
 if (process.env.NODE_ENV == 'production'){
+    
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
+
     plugins.push(new babiliPlugin());
     
     plugins.push(new optimizeCSSAssetsPlugin({
@@ -29,7 +36,10 @@ if (process.env.NODE_ENV == 'production'){
 }
 
 module.exports = {
-    entry : './app-src/app.js',
+    entry : {
+        app : './app-src/app.js',
+        vendor : ['jquery','bootstrap','reflect-metadata']
+    },
     output : {
         filename : 'bundle.js',
         path : path.resolve(__dirname, 'dist'),
