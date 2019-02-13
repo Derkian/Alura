f61266c4d74832963aba5bfc25ee005a3a97396f
Derkian <rderkian@gmail.com>
2019-02-13 18:15:49 -0200

    Instalura

---
+++ REACT/instalura/src/componentes/Foto.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+
+class FotoAtualizacoes extends Component {
+    render(){
+        return (
+            <section className="fotoAtualizacoes">
+              <a href="#top" className="fotoAtualizacoes-like">Likar</a>
+              <form className="fotoAtualizacoes-form">
+                <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo"/>
+                <input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit"/>
+              </form>
+            </section>            
+        );
+    }
+}
+
+class FotoInfo extends Component {
+    render(){
+        return (
+            <div className="foto-in fo">
+              <div className="foto-info-likes">
+
+                <a href="#top">
+                  alots_ssa
+                </a>
+
+                ,
+
+                <a href="#top">
+                  rafael_rollo
+                </a> 
+
+                 curtiram
+             
+              </div>
+
+              <p className="foto-info-legenda">
+                <a className="foto-info-autor">autor </a>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, illo?
+              </p>
+
+              <ul className="foto-info-comentarios">
+                <li className="comentario">
+                  <a className="foto-info-autor">seguidor </a>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ad, molestiae.
+                </li>
+                <li className="comentario">
+                  <a className="foto-info-autor">seguidor </a>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt cumque earum molestias voluptatem modi nihil sit magnam ratione eveniet distinctio magni error asperiores dignissimos tempora expedita, laborum ex soluta hic maiores veritatis deserunt.
+                </li>
+                <li className="comentario">
+                  <a className="foto-info-autor">seguidor </a>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum laudantium quae ab fuga odio delectus maiores voluptatibus sit commodi quidem.
+                </li>
+              </ul>
+            </div>            
+        );
+    }
+}
+
+class FotoHeader extends Component {
+    render(){
+        return (
+            <header className="foto-header">
+              <figure className="foto-usuario">
+                <img src="https://avatars0.githubusercontent.com/u/42281047?s=40&v=4" alt="foto do usuario"/>
+                <figcaption className="foto-usuario">
+                  <a href="#">
+                    Derks
+                  </a>  
+                </figcaption>
+              </figure>
+              <time className="foto-data">03/10/2016 20:13</time>
+            </header>            
+        );
+    }
+}
+
+export default class Foto extends Component {
+    render(){
+        return (
+          <div className="foto">
+            <FotoHeader/>
+            <img alt="foto" className="foto-src" src="https://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg"/>
+            <FotoInfo/>
+            <FotoAtualizacoes/>
+          </div>            
+        );
+    }
+}
