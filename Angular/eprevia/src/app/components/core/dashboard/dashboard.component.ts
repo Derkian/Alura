3b4ba5a748eb446653f87c2f280af69c59ad4ac0
Rodrigo Derkian Souza dos Santos <Rodrigo.Santos@audatex.com.br>
2020-07-23 09:50:15 -0300

    Card Layout

--- Angular/eprevia/src/app/components/core/dashboard/dashboard.component.ts
+++ Angular/eprevia/src/app/components/core/dashboard/dashboard.component.ts
@@ -20,7 +20,7 @@ export class DashboardComponent implements OnInit {
   content : Content[] = [
     { title: 'Em Andamento', iconLeft: 'query_builder', bgColor : '#1dbd55', totalRegister : 141, count : 3 },
     { title: 'Concluído', iconLeft: 'assignment_turned_in', bgColor : '#d63461', totalRegister : 26, count : 10 },
-    { title: 'Sincronizado', iconLeft: 'autorenew', bgColor : '#5c67a5', totalRegister : 12, count :5 },      
+    { title: 'Sincronizado', iconLeft: 'autorenew', bgColor : '#5c67a5', totalRegister : 12, count :5 }    
   ];
 
   constructor() { }
