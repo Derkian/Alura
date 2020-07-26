440fafccb0c1097bc1d03ae50db422e668e3d9c9
Rodrigo Derkian Souza dos Santos <Rodrigo.Santos@audatex.com.br>
2020-07-26 15:18:33 -0300

    canActive Routing

--- Angular/angular-router-sample/src/app/app.module.ts
+++ Angular/angular-router-sample/src/app/app.module.ts
@@ -3,29 +3,33 @@ import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent }     from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-
-import { HeroesModule }     from './heroes/heroes.module';
-import { CrisisCenterModule } from "./crisis-center/crisis-center.module";
-
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppComponent }            from './app.component';
+import { PageNotFoundComponent }   from './page-not-found/page-not-found.component';
 import { ComposeMessageComponent } from './compose-message/compose-message.component';
 
+import { AppRoutingModule }        from './app-routing.module';
+import { CrisisCenterModule } from "./crisis-center/crisis-center.module";
+import { AdminModule } from "./admin/admin.module";
+import { HeroesModule }            from './heroes/heroes.module';
+import { AuthModule }              from './auth/auth.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,  
     BrowserAnimationsModule,
+    FormsModule,
+    AuthModule,
     HeroesModule,
     CrisisCenterModule,
-    AppRoutingModule
+    AdminModule,
+    AppRoutingModule,
   ],
   declarations: [
-    AppComponent,    
-    PageNotFoundComponent, ComposeMessageComponent,    
+    AppComponent,
+    ComposeMessageComponent,
+    PageNotFoundComponent
   ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
