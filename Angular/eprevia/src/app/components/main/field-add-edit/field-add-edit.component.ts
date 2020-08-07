31d8f30cfbbb3ba98b332832f47d171ccae824f1
Rodrigo Derkian Souza dos Santos <Rodrigo.Santos@audatex.com.br>
2020-08-07 15:59:54 -0300

    field form

--- Angular/eprevia/src/app/components/main/field-add-edit/field-add-edit.component.ts
+++ Angular/eprevia/src/app/components/main/field-add-edit/field-add-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { slideInOutAnimation  } from "../../../animations/slide-in-out.animation";;
+import { FormGroup, FormControl  } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-field-add-edit',
@@ -10,7 +12,13 @@ import { slideInOutAnimation  } from "../../../animations/slide-in-out.animation
 })
 export class FieldAddEditComponent implements OnInit {
 
-  constructor() { }
+  fieldForm = new FormGroup({
+    label : new FormControl(''),
+    key : new FormControl(''),
+    Required : new FormControl()
+  });
+
+  constructor(public router : Router) { }
 
   ngOnInit(): void {
   }
