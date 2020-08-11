884160ffd76cf598e9cd218289527b728aa4b79d
Rodrigo Derkian Souza dos Santos <Rodrigo.Santos@audatex.com.br>
2020-08-11 14:17:19 -0300

    Field Add

--- Angular/eprevia/src/app/components/main/admin/admin.component.ts
+++ Angular/eprevia/src/app/components/main/admin/admin.component.ts
@@ -3,6 +3,11 @@ import { Router  } from "@angular/router";
 import { BaseElement } from 'src/app/element-types/base-element';
 import { TextboxElement } from 'src/app/element-types/textbox-element';
 import { FormGroup } from '@angular/forms';
+import { FormControlServiceService } from 'src/app/services/form-control-service.service';
+import { CheckboxElement } from 'src/app/element-types/checkbox-element';
+import { RadioElement } from 'src/app/element-types/radio-element';
+import { TextAreaElement } from 'src/app/element-types/textarea-element';
+import { DropdownElement } from 'src/app/element-types/dropdown-element';
 
 @Component({
   selector: 'app-admin',
@@ -21,20 +26,43 @@ export class AdminComponent implements OnInit {
                           { icon : 'call', type : 'Phone' },
                         ];
 
-                        
-  item : BaseElement<string> = new TextboxElement();
+
+  form: FormGroup;
   
-  formElements : any[] = [];
+  formElements : BaseElement<string>[] = [];
 
-  constructor(public router : Router) { }
+  constructor(public router : Router, private qcs: FormControlServiceService) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 
   addNewElement(type : string) : void {
 
-    this.formElements.push( { type : type } )
+    switch (type) {
+      case 'Radio':
+        this.formElements.push(new RadioElement( { label : 'Radio' ,  options : [  { key : 'Sim', value : 'Sim' } ,  { Key : 'Não', value : 'Não' } ] }));        
+        break;
+      case 'Toggle':
+        this.formElements.push(new CheckboxElement( { label : 'Toggle'} ));        
+        break;
+      case 'TextArea':
+        this.formElements.push(new TextAreaElement( { label : 'TextArea'} ));        
+        break;
+      case 'DropDown':
+        this.formElements.push(new DropdownElement({ label : 'DropDown', options : [  { key : '1', value : 'Opção 1' } , { key : '2', value : 'Opção 2' } ] } ));        
+        break;
+      case 'Date':
+        this.formElements.push(new TextboxElement({ label : 'Date' , type : 'date' } ));        
+        break;
+      case 'Phone':
+        this.formElements.push(new TextboxElement({ label : 'Phone' , mask : '(00) 0 0000-0000' } ));        
+        break;
+      default:
+
+        break;
+    }    
+
+    this.form = this.qcs.toFormGroup(this.formElements);    
 
   }
 
